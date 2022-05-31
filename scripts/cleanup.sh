ac931c73a862ed8b7f7ffa5445bb076e885d08e4
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2022-05-31 19:02:36 +0400

    Adds cleanup script. Minor tweaks in the README

---
+++ scripts/cleanup.sh
@@ -0,0 +1,1 @@
+kubectl delete ns --force --grace-period 0 ironman vision spiderman adhoc-team-1 captainamerica antman hawkeye adhoc-team-2
--- scripts/populate-data.sh
+++ scripts/populate-data.sh
@@ -3,26 +3,20 @@
 ### Iron Man Team
 kubectl create ns ironman
 kubectl create ns vision
-kubectl create ns spider-man
+kubectl create ns spiderman
 kubectl create ns adhoc-team-1
 
 ### Captain America Team
-kubectl create ns captain-america
-kubectl create ns ant-man
-kubectl create ns hawk-eye
+kubectl create ns captainamerica
+kubectl create ns antman
+kubectl create ns hawkeye
 kubectl create ns adhoc-team-2
-
-### Giantswarm infra
-kubectl create ns gswarm-critical
-kubectl create ns gswarm-lambda
 
 for i in {1..3}; do kubectl run ironman-$RANDOM --image nginx --namespace ironman; done
 for i in {1..3}; do kubectl run vision-$RANDOM --image nginx --namespace vision; done
-for i in {1..3}; do kubectl run spider-man-$RANDOM --image nginx --namespace spider-man; done
-for i in {1..5}; do kubectl run adhoc-team-1-$RANDOM --image nginx --namespace adhoc-team-1; done
-for i in {1..3}; do kubectl run captain-america-$RANDOM --image nginx --namespace captain-america; done
-for i in {1..5}; do kubectl run adhoc-team-2-$RANDOM --image nginx --namespace adhoc-team-2; done
-for i in {1..3}; do kubectl run hawk-eye-$RANDOM --image nginx --namespace hawk-eye; done
-for i in {1..3}; do kubectl run ant-man-$RANDOM --image nginx --namespace ant-man; done
-for i in {1..5}; do kubectl run gswarm-critical-$RANDOM --image nginx --namespace gswarm-critical; done
-for i in {1..5}; do kubectl run gswarm-lambda-$RANDOM --image nginx --namespace gswarm-lambda; done
+for i in {1..3}; do kubectl run spiderman-$RANDOM --image nginx --namespace spiderman; done
+for i in {1..3}; do kubectl run adhoc-team-1-$RANDOM --image nginx --namespace adhoc-team-1; done
+for i in {1..3}; do kubectl run cap-$RANDOM --image nginx --namespace captainamerica; done
+for i in {1..3}; do kubectl run adhoc-team-2-$RANDOM --image nginx --namespace adhoc-team-2; done
+for i in {1..3}; do kubectl run hawkeye-$RANDOM --image nginx --namespace hawkeye; done
+for i in {1..3}; do kubectl run antman-$RANDOM --image nginx --namespace antman; done
