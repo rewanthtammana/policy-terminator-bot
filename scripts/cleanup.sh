8c8d9d99f812efeb175620e701beb5b0246c6396
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2022-06-02 13:43:03 +0400

    Updates cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,1 +1,9 @@
-kubectl delete ns --force --grace-period 0 ironman vision spiderman adhoc-team-1 captainamerica antman hawkeye adhoc-team-2
+kubectl delete po --all --force --grace-period 0 --namespace ironman
+kubectl delete po --all --force --grace-period 0 --namespace vision
+kubectl delete po --all --force --grace-period 0 --namespace spiderman
+kubectl delete po --all --force --grace-period 0 --namespace captainamerica
+kubectl delete po --all --force --grace-period 0 --namespace antman
+kubectl delete po --all --force --grace-period 0 --namespace hawkeye
+kubectl delete po --all --force --grace-period 0 --namespace adhoc-team-1
+kubectl delete po --all --force --grace-period 0 --namespace adhoc-team-2
+
